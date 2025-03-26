e392f6099ed761af6eeddff3d0fe8dc20025030c
“Eslam“ <aslamalmswdya@gmail.com>
2025-03-26 22:48:39 +0200

    input validations & axios instances separation

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,18 +1,18 @@
-
 import axios, { AxiosInstance } from "axios";
 import { BASE_URL } from "./apiUrls";
 
-const axiosInstance: AxiosInstance = axios.create({
+// Create an instance for public requests (does not require authentication)
+export const publicInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
-// headers
-export const HEADERS = {
-  headers: { Authorization: localStorage.getItem("token") },
-};
+// Create an instance for private requests (requires authentication)
+export const privateInstance: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+});
 
-// axios interceptors
-axiosInstance.interceptors.request.use((config) => {
+// Add an interceptor to the private instance to automatically include the Authorization header
+privateInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
