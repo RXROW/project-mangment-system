1e08ab76e6bf78c926d254a316629a388fd7ff16
Mohamed-Khaled-Kamal <mo7med.khaled22@gmail.com>
2025-03-27 13:29:29 +0200

    auth-login

--- src/modules/shared/AuthLayout/AuthLayout.tsx
+++ src/modules/shared/AuthLayout/AuthLayout.tsx
@@ -1,12 +1,27 @@
 import { Outlet } from "react-router-dom";
+import authlogo from '../../../assets/Images/authlogo.png'
 
 export default function AuthLayout() {
 
 
   return (
-<>
+    <>
 
-<Outlet/>
+<div className="auth-container">
+  <div className="container-fluid">
+    <div className="row vh-100 justify-content-center align-items-center">
+      <div className="col-md-5 d-flex flex-column align-items-center ">
+        <img className="w-50 mb-3" src={authlogo} alt="Logo" />
+        <div className="from-container w-100 rounded-4 p-3">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 </>
   );
 }
